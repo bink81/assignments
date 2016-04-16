7ebc41cbdea7f68c838aa499cab3f55aa5e9ff47
Robert Marzeta <bink81@gmail.com>
2016-04-16 15:50:21 +0200

    Improve efficiency

--- src/main/java/GreatesCommonDivisor.java
+++ src/main/java/GreatesCommonDivisor.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 
 public class GreatesCommonDivisor {
 	public static int gcd(int a, int b) {
-		int current_gcd = 1;
+		return recursiveGcd(a, b, 1);
+	}
+
+	private static int recursiveGcd(int a, int b, int currentGcd) {
 		for (int d = 2; d <= a && d <= b; ++d) {
 			if (a % d == 0 && b % d == 0) {
-				if (d > current_gcd) {
-					current_gcd = d;
-				}
+				return recursiveGcd(a / d, b / d, currentGcd * d);
 			}
 		}
-
-		return current_gcd;
+		return currentGcd;
 	}
 
 	public static void main(String args[]) {
