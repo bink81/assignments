08c078dbaeaa843efe095a671a84deae58ec4680
Robert Marzeta <bink81@gmail.com>
2016-04-24 12:14:07 +0200

    Implement algorithm with nlogn running time

---
+++ src/test/java/FractionalKnapsackTest.java
@@ -0,0 +1,75 @@
+import java.util.List;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class FractionalKnapsackTest {
+
+	private static final double DELTA = 0.0001;
+	private static final int ZERO_SPACE = 0;
+	private static final int SOME_SPACE = 10;
+
+	@Test
+	public void testZeroSpace() throws Exception {
+		int[] values = new int[] {};
+		int[] weights = new int[] {};
+		double actual = FractionalKnapsack.getOptimalValue(ZERO_SPACE, values, weights);
+
+		Assert.assertEquals(0, actual, DELTA);
+	}
+
+	@Test
+	public void testSomeSpaceButItemsWithNoWeight() throws Exception {
+		int[] values = new int[] { 500 };
+		int[] weights = new int[] { 0 };
+		double actual = FractionalKnapsack.getOptimalValue(SOME_SPACE, values, weights);
+
+		Assert.assertEquals(0, actual, DELTA);
+	}
+
+	@Test
+	public void testSomeSpaceButNoItems() throws Exception {
+		int[] values = new int[] {};
+		int[] weights = new int[] {};
+		double actual = FractionalKnapsack.getOptimalValue(SOME_SPACE, values, weights);
+
+		Assert.assertEquals(0, actual, DELTA);
+	}
+
+	@Test
+	public void test50() throws Exception {
+		int[] values = new int[] { 60, 100, 120 };
+		int[] weights = new int[] { 20, 50, 30 };
+		double actual = FractionalKnapsack.getOptimalValue(50, values, weights);
+
+		Assert.assertEquals(180, actual, DELTA);
+	}
+
+	@Test
+	public void test10() throws Exception {
+		int[] values = new int[] { 500 };
+		int[] weights = new int[] { 30 };
+		double actual = FractionalKnapsack.getOptimalValue(10, values, weights);
+
+		Assert.assertEquals(166.6667, actual, DELTA);
+	}
+
+	@Test
+	public void testRandomValues() throws Exception {
+		final int recordSize = 1000;
+		int[] values = generateRandomValues(recordSize);
+		int[] weights = generateRandomValues(recordSize);
+
+		FractionalKnapsack.getOptimalValue(100, values, weights);
+	}
+
+	private int[] generateRandomValues(int recordSize) {
+		List<Integer> randomNumbers = new RandomArrayGenerator(recordSize, 100).generateRandomNumbers();
+		int[] values = new int[recordSize];
+		for (int i = 0; i < values.length; i++) {
+			values[i] = randomNumbers.get(i);
+		}
+		return values;
+	}
+}
